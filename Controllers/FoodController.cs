8b8be2be63a487c1e2f0415599317907c4aeca24
AV-Felipe <aerogrado@gmail.com>
2021-06-29 12:07:44 -0300

    xml swagger documentation

--- Controllers/FoodController.cs
+++ Controllers/FoodController.cs
@@ -21,7 +21,9 @@ namespace CrudMongo.Controllers
             _foodServ = foodService;
         }
 
-        //List all food elements
+        /// <summary>
+        /// Buscar todos os documentos cadastrados no banco
+        /// </summary>
         [HttpGet]
         public ActionResult<List<Food>> Read()
         {
@@ -29,9 +31,14 @@ namespace CrudMongo.Controllers
         }
         
 
-        //find an especific food element by its custom foodId
+        /// <summary>
+        /// Buscar um documento pelo seu guid FoodId
+        /// </summary>
+        /// <param name="id">FooId do documento buscado</param>
+        /// <response code="200">Retorna o documento filtrado</response>
+        /// <response code="204">Caso não haja qualquer documento com o id informado</response>
         [HttpGet("{id:guid}")]
-        public ActionResult<Food> Find(Guid id)
+        public ActionResult<Food> Find([FromRoute]Guid id)
         {
             var food = _foodServ.Find(id);
             if (food == null) return NotFound();
@@ -39,18 +46,28 @@ namespace CrudMongo.Controllers
         }
 
         
-        //creates a new food element
+        /// <summary>
+        /// Inserir um novo documento
+        /// </summary>
+        /// <param name="food">benefits é uma string onde cada palavra, separada por um espaço de outra, será transposta para um endereço dentro de uma array, no documento, </param>
+        /// <response code="200">Caso o documento seja inserido com sucesso</response>
         [HttpPost]
-        public ActionResult<Food> Create(FoodInputDTO food)
+        public ActionResult<Food> Create([FromBody]FoodInputDTO food)
         {
             _foodServ.Create(food);
             return Ok(food);
         }
         
 
-        //updates all fields of a food element
+        /// <summary>
+        /// Atualizar um documento já cadastrado
+        /// </summary>
+        /// /// <param name="id">FoodId do documento a ser atualizado</param>
+        /// <param name="foodIn">Novos dados para atualizar o documento indicado</param>
+        /// <response code="200">Cao o documento seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um documento com este FoodId</response>
         [HttpPut("{id:guid}")]
-        public ActionResult Update([FromRoute] Guid id, FoodInputDTO foodIn)
+        public ActionResult Update([FromRoute] Guid id, [FromBody]FoodInputDTO foodIn)
         {
             var food = _foodServ.Find(id);
 
@@ -61,7 +78,12 @@ namespace CrudMongo.Controllers
             return Ok();
         }
 
-        //deletes one food element
+        /// <summary>
+        /// Excluir um documento
+        /// </summary>
+        /// /// <param name="id">FoodId do documento a ser excluído</param>
+        /// <response code="200">Caso o o documento tenha sido excluído</response>
+        /// <response code="404">Caso não exista um documento com este FoodId</response>
         [HttpDelete("{id:guid}")]
         public ActionResult Delete([FromRoute]Guid id)
         {
@@ -74,9 +96,14 @@ namespace CrudMongo.Controllers
             return Ok();
         }
 
-        //teste adicionar benefício
+        /// <summary>
+        /// Adicionar um novo item ao array benefits de um documento já criado
+        /// </summary>
+        /// /// <param name="id">FoodId do documento que receberá um novo benefício</param>
+        /// <param name="benefit">Novo benefício que será adicionado à array já existente</param>
+        /// <response code="200">Caso o benefício seja adicionado com sucesso</response>
         [HttpPatch("{id:guid}")]
-        public ActionResult AddBenefit([FromRoute]Guid id, string benefit)
+        public ActionResult AddBenefit([FromRoute]Guid id, [FromQuery]string benefit)
         {
             var food = Find(id);
             _foodServ.AddBenefit(id, benefit);
--- README.md
+++ README.md
@@ -8,5 +8,11 @@ API para cadastro e acesso de itens em um banco de dados com informações de al
 
 Por se tratar de um projeto pedagógico, fez-se uso abundante de comentários, evidenciando detalhes já explícitos no código.
 
+Buscamos explorar a versatilidade do padrão NoSQL do Mongo, explorando o uso de matrizes com variados comprimentos dentro dos documentos da coleção.
+
+Essencialmente, esta API se conecta a uma instância local do MongoDB, nela ela adiciona, documentos com três campos string informados pelo usuário. O campo "Benefits" recebe uma string onde cada espaço é considerado como uma divisão, assim, cada palavra será transposta para um diferente campo, em uma matriz, no documento do banco.
+
+A API cuida da datação da criação e alteração das entradas, além de gerar um id do tipo GUID, para evitar a manipulação do _id gerado pelo MongoDB.
+
 
  
